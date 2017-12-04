a333a20cd6edc6833168dce705aea06b7431024e
swayvil <swayvil@gmail.com>
2017-12-04 17:09:08 +0100

    fixes

--- activity/mqtt-client/activity.go
+++ activity/mqtt-client/activity.go
@@ -3,19 +3,19 @@ package mqttclient
 import (
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 var log = logger.GetLogger("activity-mqtt-client")
 
 // MQTTClientActivity is a stub for your Activity implementation
 type MQTTClientActivity struct {
-	metadata   *activity.Metadata
-	mqttClient *MQTTClient
+	metadata *activity.Metadata
 }
 
 // NewActivity creates a new activity
 func NewActivity(metadata *activity.Metadata) activity.Activity {
-	return &MQTTClientActivity{metadata, nil}
+	return &MQTTClientActivity{metadata: metadata}
 }
 
 // Metadata implements activity.Activity.Metadata
@@ -31,9 +31,8 @@ func (a *MQTTClientActivity) Eval(context activity.Context) (done bool, err erro
 	qos := context.GetInput("qos").(string)
 	topic := context.GetInput("topic").(string)
 	msg := context.GetInput("message").(string)
-	
-	a.mqttClient = NewMQTTClient(brokerUrl, clientId, qos)
-	a.mqttClient.Publish(topic, msg)
+
+	a.publish(brokerUrl, clientId, qos, topic, msg)
 
 	log.Debugf("Message published on [%s] topic, [%s] MQTT broker", topic, brokerUrl)
 
@@ -42,4 +41,28 @@ func (a *MQTTClientActivity) Eval(context activity.Context) (done bool, err erro
 
 	// Signal to the Flogo engine that the activity is completed
 	return true, nil
+}
+
+// *************
+// MQTT specific
+// *************
+
+func (a *MQTTClientActivity) publish(brokerUrl string, clientId string, qos string, topic string, msg string) {
+	opts := mqtt.NewClientOptions().AddBroker(brokerUrl)
+	opts.SetClientID(clientId)
+
+	// Create and start a client using the above ClientOptions
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	token := client.Publish(topic, 0, false, msg)
+	token.Wait()
+
+	// Disconnect
+	if token := client.Unsubscribe(topic); token.Wait() && token.Error() != nil {
+		log.Error(token.Error())
+	}
+	client.Disconnect(250)
 }
