33d185209c4441cb7777e058d3b06bae6a58a75d
swayvil <swayvil@gmail.com>
2017-12-04 15:56:10 +0100

    Add sources

---
+++ activity/mqtt-client/activity.go
@@ -0,0 +1,45 @@
+package mqttclient
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+var log = logger.GetLogger("activity-mqtt-client")
+
+// MQTTClientActivity is a stub for your Activity implementation
+type MQTTClientActivity struct {
+	metadata   *activity.Metadata
+	mqttClient *MQTTClient
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MQTTClientActivity{metadata: metadata, nil}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MQTTClientActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MQTTClientActivity) Eval(context activity.Context) (done bool, err error) {
+	// Get the activity data from the context
+	brokerUrl := context.GetInput("brokerUrl").(string)
+	clientId := context.GetInput("clientId").(string)
+	qos := context.GetInput("qos").(string)
+	topic := context.GetInput("topic").(string)
+	msg := context.GetInput("message").(string)
+	
+	a.mqttClient = NewMQTTClient(brokerUrl, clientId, qos)
+	a.mqttClient.Publish(topic, msg)
+
+	log.Debugf("Message published on [%s] topic, [%s] MQTT broker", topic, brokerUrl)
+
+	// Set the result as part of the context
+	//context.SetOutput("message", "The Flogo engine says "+salutation+" to "+name)
+
+	// Signal to the Flogo engine that the activity is completed
+	return true, nil
+}
---
+++ activity/mqtt-client/mqtt-client.go
@@ -0,0 +1,57 @@
+package mqttclient
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+type MQTTClient struct {
+	context   *activity.Context
+	client    mqtt.Client
+	brokerUrl string
+	clientId  string
+	qos       byte
+	topic     string
+}
+
+var log = logger.GetLogger("activity-mqtt-client")
+
+// Default message handler function
+//func msgHandler(client mqtt.Client, msg mqtt.Message) {
+//	log.Infof("TOPIC: %s\n", msg.Topic())
+//	log.Infof("MSG: %s\n", msg.Payload())
+//}
+
+func NewMQTTClient(context activity.Context, brokerUrl string, clientId string, qos byte) *MQTTClient {
+	opts := mqtt.NewClientOptions().AddBroker(brokerUrl)
+	opts.SetClientID(clientId)
+//	opts.SetDefaultPublishHandler(msgHandler)
+
+	// Create and start a client using the above ClientOptions
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	return &MQTTClient{client, brokerUrl, brokerUrl, qos, ""}
+}
+
+//func (mqttClient *MQTTClient) Subscribe(topic string) {
+//	if token := mqttClient.client.Subscribe(topic, mqttClient.qos, nil); token.Wait() && token.Error() != nil {
+//		log.Error(token.Error())
+//	}
+//}
+
+func (mqttClient *MQTTClient) Publish(topic string, msg string) {
+	mqttClient.topic = topic
+	token := mqttClient.client.Publish(topic, mqttClient.qos, false, msg)
+	token.Wait()
+}
+
+func (mqttClient *MQTTClient) Disconnect() {
+	if mqttClient.topic != "" {
+		if token := mqttClient.client.Unsubscribe(mqttClient.topic); token.Wait() && token.Error() != nil {
+			log.Error(token.Error())
+		}
+	}
+	mqttClient.client.Disconnect(250)
+}
